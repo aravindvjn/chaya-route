29c961f1e7356b0ecc864ce69f9f104af561f801
aravind <aravind284479@gmail.com>
2025-05-27 22:03:28 +0530

    feat: Initial setup

---
+++ front-end/app/(tabs)/index.tsx
@@ -0,0 +1,11 @@
+
+import { Text, View } from 'react-native';
+
+
+export default function HomeScreen() {
+  return (
+    <View className='h-full bg-white'>
+      <Text className='text-white-500'>Hai</Text>
+    </View>
+  );
+}
